75f54e9ac0c2c7323d1547670131132b645611d3
Carson Haskell <carson.haskell@gmail.com>
2023-08-10 17:13:38 -0700

    finish portfolio page

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,8 +1,44 @@
 import React from 'react';
 import styles from './page.module.css';
+import Image from 'next/image';
+import Button from '@/components/Button';
 
-function Category() {
-  return <div className={styles.container}>Category</div>;
+function Category({ params }) {
+  return (
+    <div>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Item 1</h1>
+          <p className={styles.description}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+            facere at voluptate. Necessitatibus fugit placeat quam nisi,
+            explicabo dicta quos rerum rem neque ullam enim, maiores amet
+            commodi asperiores velit.
+          </p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image src="/contact.png" fill={true} alt="" className={styles.img} />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Item 2</h1>
+          <p className={styles.description}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+            ratione minima tempore saepe corrupti perspiciatis nam nemo quia
+            quaerat amet pariatur, doloribus rem veniam esse blanditiis cum.
+            Nemo, nulla quae.
+          </p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image src="/contact.png" fill={true} alt="" className={styles.img} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Category;
